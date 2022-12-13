2eee73291ef52f9b13a3251362c31fa2e9cb856d
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2022-12-13 09:51:31 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d",&a);     if(a%4==0)     {         printf("%d",a/4);
        }
        else
        {
            printf("%d",(a/4)+1);
        }
    }

---
+++ Cars.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+        printf("%d",a/4);
+        
+    }
+    else
+    {
+        printf("%d",(a/4)+1);
+    }
+}
