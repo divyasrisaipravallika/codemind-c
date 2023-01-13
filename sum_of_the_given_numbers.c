c19742360466c00bbdfaaf741590ff2d686d836f
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-01-13 09:30:34 +0530

    #include<stdio.h> int main() {     int n,a,i,b,s;     scanf("%d",&n);     while(n--)     {
                scanf("%d%d",&a,&b);
                s=a+b;
                printf("%d
    ",s);
            //printf("%d
    ",s);
        
        }
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i,b,s;
+    scanf("%d",&n);
+    while(n--)
+    {
+        
+            scanf("%d%d",&a,&b);
+            s=a+b;
+            printf("%d
+",s);
+        //printf("%d
+",s);
+    
+    }
+}
