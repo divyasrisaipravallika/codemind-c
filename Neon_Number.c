d4dc29b59d3fe1009467750f8b46f603b517ccba
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-01-15 20:43:30 +0530

    #include<stdio.h> int main() { int i,r=0,m=0,n; scanf("%d",&i); n=i*i; while(n!=0) { m=n%10; r=r+m; n/=10; } if(r==i) { printf("Neon Number"); } else { printf("Not Neon Number"); }}

---
+++ Neon_Number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ int i,r=0,m=0,n;
+scanf("%d",&i); 
+n=i*i;
+while(n!=0)
+{ m=n%10; r=r+m; n/=10; } 
+if(r==i) { printf("Neon Number"); }
+else { printf("Not Neon Number"); }}
