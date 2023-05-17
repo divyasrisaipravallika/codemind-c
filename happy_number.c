08b66c9cf3204430f19b5a06a688419503edc197
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-17 22:58:40 +0530

    #include<stdio.h> #include<math.h> int fun(int n) {     int s=0,r,res;     while(1)     {         s=0;         while(n!=0)         {             r=n%10;             s=s+(r*r);             n=n/10;         }         if(s>9)         {             n=s;             continue;         }         else         {             break;         }     }     if(s==1 || s==7)     {         res=1;
        }
        else
        {
            res=0;
        }
        return res;
    }
    int main()
    {
        int n;
        scanf("%d",&n);
        int resi;
        resi=fun(n);
        if(resi==1)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

--- happy_number.c
+++ happy_number.c
@@ -1,27 +1,50 @@
 #include<stdio.h>
 #include<math.h>
+int fun(int n)
+{
+    int s=0,r,res;
+    while(1)
+    {
+        s=0;
+        while(n!=0)
+        {
+            r=n%10;
+            s=s+(r*r);
+            n=n/10;
+        }
+        if(s>9)
+        {
+            n=s;
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if(s==1 || s==7)
+    {
+        res=1;
+        
+    }
+    else
+    {
+        res=0;
+    }
+    return res;
+}
 int main()
 {
-    int i,j,n,s=0;
+    int n;
     scanf("%d",&n);
-    while(s!=1 && s!=4)
+    int resi;
+    resi=fun(n);
+    if(resi==1)
     {
-      s=0;
-      while(n>0)
-      {
-          j=n%10;
-          s+=j*j;
-          n=n/10;
-        
-      }
-      n=s;
+        printf("True");
     }
-  if(s==1)
-  {
-      printf("True");
-  }
-  else
-  {
-      printf("False");
-  }
+    else
+    {
+        printf("False");
+    }
 }
