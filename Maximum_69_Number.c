a3323ea271474cc09d35f9b8109333a82f82cc09
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-18 22:12:12 +0530

    #include<stdio.h> int main() {     int n,d,i=0,j;     scanf("%d",&n);     int a[n];     while(n!=0)     {         d=n%10;         n=n/10;         a[i]=d;         i++;
        }
        for(j=i-1;j>=0;j--) 
        {
            if(a[j]==6) 
            { 
                a[j]=9; break;
    } 
            
        }
        for(j=i-1;j>=0;j--) 
        { 
            printf("%d",a[j]); 
            
        }
        
    }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 int main()
 {
-  int n,d,i=0,j;
-  scanf("%d",&n);
-  int a[n];
-  while(n!=0)
-  {
-      d=n%10;
-      n=n/10;
-      a[i]=d;
-      i++;
-  }
-  for(j=i-1;j>=0;j--)
-  {
-      if(a[j]==6)
-      {
-          a[j]=9;
-          break;
-      }
-  }
-  for(j=i-1;j>=0;j--)
-  {
-      printf("%d",a[j]);
-  }
+    int n,d,i=0,j; 
+    scanf("%d",&n);
+    int a[n]; 
+    while(n!=0) 
+    {
+        d=n%10; 
+        n=n/10; 
+        a[i]=d;
+        i++; 
+        
+    }
+    for(j=i-1;j>=0;j--) 
+    {
+        if(a[j]==6) 
+        { 
+            a[j]=9; break;
+} 
+        
+    }
+    for(j=i-1;j>=0;j--) 
+    { 
+        printf("%d",a[j]); 
+        
+    }
+    
 }
