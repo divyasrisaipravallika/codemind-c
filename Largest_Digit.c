8cf28fb0495620783ed7734e7459e27419a8fe9c
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-19 19:06:04 +0530

    #include<stdio.h> int main() {     int n,i,m=0,r=0;     scanf("%d",&n);     for(i=1;n>i;i++)     {         r=n%10;         n=n/10;         if(m<r)         {             m=r;         }     }
        printf("%d",m);
        
    }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
-{
-    int n,i,m=0,r=0;
-    scanf("%d",&n);
+{ 
+    int n,i,m=0,r=0; 
+    scanf("%d",&n); 
     for(i=1;n>i;i++)
-    {
+    { 
         r=n%10;
-        n=n/10;
+        n=n/10; 
         if(m<r)
         {
             m=r;
         }
     }
+      
+    
     printf("%d",m);
+    
 }
