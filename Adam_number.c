174b7e5d9b5449e66e6fa373195d6d4f2a137a82
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-19 19:31:06 +0530

    #include<stdio.h> int main() {     int n,i,m=0,r=0,o=0,g=0;     scanf("%d",&n);     int d;     d=n;     while(n!=0)     {         m=n%10;         r=r*10+m;         n/=10;     }     int c;     c=r*r;     while(c!=0)     {         g=c%10;         o=o*10+g;         c/=10;     }     int k;     k=d*d;     if(o==k)    {     printf("True");
       }
       else
       { 
           printf("False"); 
           
       }
        
    }

--- Adam_number.c
+++ Adam_number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,m=0,r=0,o=0,g=0;
+    int n,i,m=0,r=0,o=0,g=0; 
     scanf("%d",&n);
     int d;
     d=n;
     while(n!=0)
-    {
-        m=n%10;
+    { 
+        m=n%10; 
         r=r*10+m;
         n/=10;
-    }
+    } 
     int c;
     c=r*r;
     while(c!=0)
@@ -18,15 +18,18 @@ int main()
         g=c%10;
         o=o*10+g;
         c/=10;
-    }
+    } 
     int k;
     k=d*d;
-    if(o==k)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+    if(o==k) 
+   {
+    printf("True"); 
+       
+   }
+   else
+   { 
+       printf("False"); 
+       
+   }
+    
 }
