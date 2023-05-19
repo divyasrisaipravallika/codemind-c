f8feed806a9c60639b46d05fce2ac56e8c9c55f1
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-19 19:16:36 +0530

    #include<stdio.h> int main() {     int i,n,m;     scanf("%d%d",&m,&n);     int gcd;     for(i=1;i<=m&&i<=n;i++)
     {
         if(m%i==0&&n%i==0)
         {
             gcd=i; 
             
         }
    } 
    printf("%d",gcd);
        
    }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
-{ int i,n,m; 
-scanf("%d%d",&m,&n); 
-int gcd; 
-for(i=1;i<=m&&i<=n;i++) 
-{ if(m%i==0&&n%i==0)
-{
-    gcd=i; 
+{ 
+    int i,n,m;
+    scanf("%d%d",&m,&n);
+    int gcd;
+    for(i=1;i<=m&&i<=n;i++)
     
+ {
+     if(m%i==0&&n%i==0)
+     {
+         gcd=i; 
+         
+     }
 } 
+printf("%d",gcd);
     
-} 
-printf("%d",gcd);}
+}
